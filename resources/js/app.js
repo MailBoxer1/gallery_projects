aa6c4977972fffd54fe01fccbabf8a236567fbce
GoldAbsolute <sokoldima1998@mail.ru>
2023-07-09 14:15:08 +0300

    Установка и подключение зависимостей vue+debugbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+
+import app from './components/ProjectGallery.vue';
+
+createApp(app).mount('#app')
---
+++ resources/js/components/ProjectGallery.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "ProjectGallery"
+}
+</script>
+
+<style scoped>
+
+</style>
